5656b27c9f73ff3ea8c39cf65d30662ae7c9bcc4
Daniel You <danielyou416@gmail.com>
2021-10-27 20:33:57 -0400

    lvim: get rid of lsp progress in lualine

--- .config/lvim/lua/user/lualine.lua
+++ .config/lvim/lua/user/lualine.lua
@@ -1,22 +1,5 @@
 local M = {}
 
-local function lsp_progress()
-  local messages = vim.lsp.util.get_progress_messages()
-  if #messages == 0 then
-    return ""
-  end
-  local status = {}
-  for _, msg in pairs(messages) do
-    table.insert(status, (msg.percentage or 0) .. "%% " .. (msg.title or ""))
-  end
-  local spinners = { "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" }
-  local ms = vim.loop.hrtime() / 1000000
-  local frame = math.floor(ms / 120) % #spinners
-  return table.concat(status, " | ") .. " " .. spinners[frame + 1]
-end
-
-vim.cmd [[autocmd User LspProgressUpdate let &ro = &ro]]
-
 local function diff_source()
   local gitsigns = vim.b.gitsigns_status_dict
   if gitsigns then
@@ -311,10 +294,6 @@ M.config = function()
     end,
     color = { fg = colors.green },
     cond = conditions.hide_in_width,
-  }
-  ins_left {
-    lsp_progress,
-    cond = conditions.hide_small,
   }
 
   -- Insert mid section. You can make any number of sections in neovim :)
