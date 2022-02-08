d49a6e20d9da0ff1688e6aea9ca93c6d9392c5e7
Daniel You <danielyou416@gmail.com>
2022-02-08 12:00:06 -0500

    change mic script for external mic

--- .local/bin/toggle_microphone.sh
+++ .local/bin/toggle_microphone.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-state=`amixer set Capture toggle | gawk 'match($0, /Front Left.*\[(.*)\]/, a) {print a[1]}'`
+state=`amixer set Capture toggle | gawk 'match($0, /Mono.*\[(.*)\]/, a) {print a[1]}'`
 notify-send "Mic $state"
