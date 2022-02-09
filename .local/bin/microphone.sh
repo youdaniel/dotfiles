d88244ccb36af1e87f245b49d0156859d17c100f
Daniel You <danielyou416@gmail.com>
2022-02-09 17:36:32 -0500

    update microphone.sh for laptop

--- .local/bin/microphone.sh
+++ .local/bin/microphone.sh
@@ -1,6 +1,14 @@
-#!/bin/sh
+#!/bin/bash
+
+if [[ "$IS_LAPTOP"=="true" ]]
+then
+  MIC="Front Left"
+else
+  MIC="Mono"
+fi
 
 case "$1" in
   "toggle") amixer set Capture toggle || exit;;
-  "status") amixer | gawk 'match($0, /Mono.*\[(.*)\]/, a) {print a[1]}' || exit;;
+  "status") amixer get Capture | gawk 'match($0, /'"$MIC"'.*\[(.*)\]/, a) {print a[1]}' || exit;;
 esac
+
