f8851ca85ff3d39915923e43b114849847be772d
Daniel You <danielyou416@gmail.com>
2022-04-04 12:06:14 -0400

    lvim: fix cursorline highlighting

--- .config/lvim/lua/user/neovim.lua
+++ .config/lvim/lua/user/neovim.lua
@@ -11,7 +11,7 @@ M.config = function()
   -- Cursorline highlighting control
   --  Only have it on in the active buffer
   vim.opt.cursorline = true -- Highlight the current line
-  if vim.fn.has "nvim-0.7" then
+  if vim.fn.has "nvim-0.7" ~= 0 then
     local group = vim.api.nvim_create_augroup("CursorLineControl", { clear = true })
     vim.api.nvim_create_autocmd("WinLeave", {
       group = group,
@@ -22,7 +22,9 @@ M.config = function()
     vim.api.nvim_create_autocmd("WinEnter", {
       group = group,
       callback = function()
-        vim.opt_local.cursorline = true
+        if vim.bo.filetype ~= "alpha" then
+          vim.opt_local.cursorline = true
+        end
       end,
     })
   end
