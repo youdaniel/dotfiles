3079644911f84424408f6515f565b828e5a0bf43
Daniel You <danielyou416@gmail.com>
2022-05-26 20:54:59 -0400

    lvim: use new autocmd api

--- .config/lvim/lua/user/autocommands.lua
+++ .config/lvim/lua/user/autocommands.lua
@@ -1,21 +1,22 @@
 local M = {}
 
+local create_aucmd = vim.api.nvim_create_autocmd
+
 M.config = function()
-  lvim.autocommands.custom_groups = {
-    { "BufNewFile", "*.cpp", "0r ~/Documents/CP/cp.cpp | $d" },
-    {
-      "FileType",
-      "cpp",
-      "nnoremap <F9> :w <bar> !g++ -std=c++17 -Wshadow -Wall % -o %:r -g -fsanitize=address -fsanitize=undefined -D_GLIBCXX_DEBUG <CR>",
-    },
-    { "FileType", "markdown", "set nowrap" },
-    { "FileType", "alpha", "nnoremap <silent> <buffer> q :q<CR>" },
-    {
-      "FileType",
-      "alpha",
-      "set laststatus=0 | set noruler | autocmd BufLeave <buffer> set laststatus=2",
-    },
-  }
+  vim.api.nvim_create_augroup("_lvim_user", {})
+  create_aucmd("BufNewFile", { group = "_lvim_user", pattern = "*.cpp", command = "0r ~/Documents/CP/cp.cpp | $d" })
+  create_aucmd("FileType", {
+    group = "_lvim_user",
+    pattern = "cpp",
+    command = "nnoremap <F9> :w <bar> !g++ -std=c++17 -Wshadow -Wall % -o %:r -g -fsanitize=address -fsanitize=undefined -D_GLIBCXX_DEBUG <CR>",
+  })
+  create_aucmd("FileType", { group = "_lvim_user", pattern = "markdown", command = "set nowrap" })
+  create_aucmd("FileType", { group = "_lvim_user", pattern = "alpha", command = "nnoremap <silent> <buffer> q :q<CR>" })
+  create_aucmd("FileType", {
+    group = "_lvim_user",
+    pattern = "alpha",
+    command = "set laststatus=0 | set noruler | autocmd BufLeave <buffer> set laststatus=2",
+  })
 end
 
 return M
