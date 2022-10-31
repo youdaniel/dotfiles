50970da6f08bd0c1b5881a8487b2d590daa3be21
Daniel You <danielyou416@gmail.com>
2022-10-31 00:00:54 -0400

    lvim: add bufferline move binding

--- .config/lvim/lua/user/keybindings.lua
+++ .config/lvim/lua/user/keybindings.lua
@@ -1,14 +1,14 @@
 local M = {}
 
 M.config = function()
-  lvim.keys.normal_mode["<S-l>"] = ":BufferLineCycleNext<CR>"
-  lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
   lvim.keys.visual_mode["<C-c>"] = '"+yi'
   lvim.keys.visual_mode["<C-v>"] = 'c<ESC>"+p'
   lvim.keys.insert_mode["<C-v>"] = '<ESC>"+pa'
 
-  lvim.keys.normal_mode["<S-l>"] = ":BufferLineCycleNext<CR>"
-  lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
+  lvim.keys.normal_mode["<S-l>"] = "<Cmd>BufferLineCycleNext<CR>"
+  lvim.keys.normal_mode["<S-h>"] = "<Cmd>BufferLineCyclePrev<CR>"
+  lvim.keys.normal_mode["[b"] = "<Cmd>BufferLineMovePrev<CR>"
+  lvim.keys.normal_mode["]b"] = "<Cmd>BufferLineMoveNext<CR>"
 
   lvim.builtin.which_key.mappings["n"] = {
     name = "Neogen",
