329b459c8cea253b8997d3c192a0cb6a622fd1e1
Daniel You <danielyou416@gmail.com>
2023-03-22 16:59:54 -0400

    move CP stuff into after/ftplugin

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -9,9 +9,6 @@ require("user.plugins").config()
 -- Builtin
 require("user.builtin").config()
 
--- Autocommands
-require("user.autocommands").config()
-
 -- Keymappings
 require("user.keybindings").config()
 
