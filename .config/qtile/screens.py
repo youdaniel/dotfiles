0a4849be3c149ba62d0155ad0dc9e0431c77bc3a
Daniel You <danielyou416@gmail.com>
2023-05-01 18:23:02 -0400

    qtile: color bar

--- .config/qtile/screens.py
+++ .config/qtile/screens.py
@@ -11,35 +11,25 @@ from extras.groupbox import GroupBox
 colors = {
     "bg": "#313244",
     "fg": "#cdd6f4",
-    "black": "#11111b",
+    "pink": "#f5c2e7",
+    "mauve": "#cba6f7",
     "red": "#f38ba8",
+    "peach": "fab387",
     "green": "#a6e3a1",
     "yellow": "#f9e2af",
     "blue": "#89b4fa",
-    "pink": "#f5c2e7",
     "teal": "#94e2d5",
     "white": "#bac2de",
 }
 transparent = "#00000000"
 
 
-def mic_status():
-    mic_status = (
-        subprocess.run(
-            [f"{os.getenv('HOME')}/.config/qtile/scripts/microphone.sh", "status"],
-            stdout=subprocess.PIPE,
-        )
-        .stdout.decode("utf-8")
-        .strip()
-    )
-    return " (M)" if mic_status == "off" else " (UM)"
-
-
 def currently_playing():
     def get_metadata(metadata_type: str):
         return (
             subprocess.run(
-                ["playerctl", "metadata", metadata_type], stdout=subprocess.PIPE
+                ["playerctl", "metadata", "-p", "spotify", metadata_type],
+                stdout=subprocess.PIPE,
             )
             .stdout.decode("utf-8")
             .strip()
@@ -52,7 +42,11 @@ def currently_playing():
     artist = (artist[:20] + "..") if len(artist) > 20 else artist
     artist = f" - {artist}" if artist else ""
 
-    return f" ♪ {title}{artist}".rstrip() if title else ""
+    return f"{title}{artist}".rstrip() if title else ""
+
+
+def music_icon():
+    return " " if len(currently_playing()) > 0 else ""
 
 
 def transparent_separator(padding=10):
@@ -73,6 +67,10 @@ def base(
         "background": bg,
         "foreground": fg,
     }
+
+
+def icon_font(bg: str = colors["bg"], fg: str = colors["fg"]) -> dict:
+    return base(font="Font Awesome 6 Free Solid", fontsize=12, padding=2, bg=bg, fg=fg)
 
 
 def rectangle(side: str = "", r: int = 10) -> dict:
@@ -98,23 +96,24 @@ def wrap_widgets(*widgets, sep=True):
     return wrapped
 
 
-def clock(format: str) -> list:
-    return wrap_widgets(modify(widget.Clock, **base(), format=format))
+def clock(icon, fmt: str, fg: str = colors["fg"], sep: bool = True) -> list:
+    return wrap_widgets(icon, modify(widget.Clock, **base(fg=fg), format=fmt), sep=sep)
 
 
-def audio() -> list:
+def audio(vol_fg=colors["teal"], music_fg=colors["green"]) -> list:
     return wrap_widgets(
-        modify(widget.Volume, **base(), fmt="Vol: {}"),
+        modify(widget.TextBox, **icon_font(fg=vol_fg), text=" "),
+        modify(widget.Volume, **base(fg=vol_fg)),
+        modify(widget.Sep, **base(fg=colors["bg"], padding=5)),
         modify(
             widget.GenPollText,
-            **base(),
+            **icon_font(fg=music_fg),
             update_interval=1,
-            func=mic_status,
-            markup=False,
+            func=music_icon,
         ),
         modify(
             widget.GenPollText,
-            **base(),
+            **base(fg=music_fg),
             update_interval=1,
             func=currently_playing,
             markup=False,
@@ -139,8 +138,8 @@ def groups():
         borderwidth=0,
         active=colors["fg"],
         inactive=colors["bg"],
-        this_current_screen_border=colors["green"],
-        this_screen_border=colors["green"],
+        this_current_screen_border=colors["red"],
+        this_screen_border=colors["red"],
         other_screen_border=colors["fg"],
         other_current_screen_border=colors["fg"],
         disable_drag=True,
@@ -148,32 +147,34 @@ def groups():
     )
 
 
-def cpu() -> list:
+def cpu(fg: str = colors["blue"]) -> list:
     return wrap_widgets(
+        modify(widget.TextBox, **icon_font(fg=fg), text=" "),
         modify(
             widget.CPU,
-            **base(),
+            **base(fg=fg),
             format="CPU {load_percent}%",
             update_interval=1,
         ),
-        modify(widget.TextBox, **base(padding=2), text="|"),
         modify(
             widget.ThermalSensor,
-            **base(),
+            **base(fg=fg),
             tag_sensor="Package id 0" if os.getenv("IS_LAPTOP") else "Tctl",
             threshold=90,
+            format=" ({temp:.1f}{unit})",
         ),
     )
 
 
-def mem():
+def mem(fg: str = colors["peach"]):
     return wrap_widgets(
+        modify(widget.TextBox, **icon_font(fg=fg), text=" "),
         modify(
             widget.Memory,
-            **base(),
+            **base(fg=fg),
             measure_mem="G",
             format="{MemUsed:.2f} GB",
-        )
+        ),
     )
 
 
@@ -189,19 +190,22 @@ def battery():
     )
 
 
-def net():
+def net(fg: str = colors["mauve"]):
     return wrap_widgets(
-        modify(
-            widget.Net,
-            **base(),
-            format="{down} ↓↑{up}",
-        )
+        modify(widget.TextBox, **icon_font(fg=fg), text=" "),
+        modify(widget.Net, **base(fg=fg), format="{down} ↓↑{up}"),
     )
 
 
 def init_widgets_list(show_systray=True):
     widgets = []
-    widgets.extend(clock("%A, %B %d"))
+    widgets.extend(
+        clock(
+            modify(widget.TextBox, **icon_font(fg=colors["yellow"]), text=" "),
+            fmt="%A, %B %d",
+            fg=colors["yellow"],
+        )
+    )
     widgets.extend(audio())
 
     if show_systray:
@@ -218,7 +222,14 @@ def init_widgets_list(show_systray=True):
     if os.getenv("IS_LAPTOP"):
         widgets.extend(battery())
 
-    widgets.extend(clock(format="%I:%M %p"))
+    widgets.extend(
+        clock(
+            modify(widget.TextBox, **icon_font(fg=colors["pink"]), text=" "),
+            fmt="%I:%M %p",
+            fg=colors["pink"],
+            sep=False,
+        )
+    )
     return widgets
 
 
