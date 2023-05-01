4ffdeb78cfe0df469a8ce4109ef2eca8f119670c
Daniel You <danielyou416@gmail.com>
2023-05-01 13:49:31 -0400

    qtile: use qtile_extras for widgets

--- .config/qtile/screens.py
+++ .config/qtile/screens.py
@@ -3,6 +3,8 @@ import subprocess
 
 from libqtile import bar, qtile, widget
 from libqtile.config import Screen
+from qtile_extras.widget import modify
+from qtile_extras.widget.decorations import RectDecoration
 
 from extras.groupbox import GroupBox
 
@@ -19,7 +21,6 @@ colors = {
     "white": "#bac2de",
 }
 transparent = "#00000000"
-widget_defaults = dict(font="UbuntuMono Nerd Font Mono", fontsize=14, padding=2)
 
 
 def mic_status():
@@ -58,45 +59,62 @@ def transparent_separator(padding=10):
     return widget.Sep(padding=padding, linewidth=0)
 
 
-def bubble_widget(*widgets, sep=True):
-    bubble_attrs = dict(
-        font="UbuntuMono Nerd Font Mono",
-        foreground=getattr(widgets[0], "background", colors["bg"]),
-        padding=0,
-        fontsize=26,
-    )
+def base(
+    font: str = "UbuntuMono Nerd Font Mono",
+    fontsize: int = 14,
+    padding: int = 0,
+    bg: str = colors["bg"],
+    fg: str = colors["fg"],
+) -> dict:
+    return {
+        "font": font,
+        "fontsize": fontsize,
+        "padding": padding,
+        "background": bg,
+        "foreground": fg,
+    }
 
-    widgets = [
-        widget.TextBox(text="\ue0b6", **bubble_attrs),
+
+def rectangle(side: str = "", r: int = 10) -> dict:
+    return {
+        "decorations": [
+            RectDecoration(
+                filled=True,
+                radius={"left": [r, 0, 0, r], "right": [0, r, r, 0]}.get(side, r),
+                use_widget_background=True,
+            ),
+        ],
+    }
+
+
+def wrap_widgets(*widgets, sep=True):
+    wrapped = [
+        modify(widget.Sep, **base(padding=8, fg=colors["bg"]), **rectangle("left")),
         *widgets,
-        widget.TextBox(text="\ue0b4", **bubble_attrs),
+        modify(widget.Sep, **base(padding=8, fg=colors["bg"]), **rectangle("right")),
     ]
-
     if sep:
-        widgets.append(transparent_separator())
+        wrapped.append(transparent_separator())
+    return wrapped
 
-    return widgets
+
+def clock(format: str) -> list:
+    return wrap_widgets(modify(widget.Clock, **base(), format=format))
 
 
-def init_widgets_list(systray=True, is_laptop=os.getenv("IS_LAPTOP")):
-    widgets = []
-    widgets.extend(
-        bubble_widget(
-            widget.Clock(**widget_defaults, background=colors["bg"], format="%A, %B %d")
-        ),
-    )
-
-    audio_widgets = [
-        widget.Volume(**widget_defaults, fmt="Vol: {}", background=colors["bg"]),
-        widget.GenPollText(
-            **widget_defaults,
+def audio() -> list:
+    return wrap_widgets(
+        modify(widget.Volume, **base(), fmt="Vol: {}"),
+        modify(
+            widget.GenPollText,
+            **base(),
             update_interval=1,
             func=mic_status,
             markup=False,
-            background=colors["bg"],
         ),
-        widget.GenPollText(
-            **widget_defaults,
+        modify(
+            widget.GenPollText,
+            **base(),
             update_interval=1,
             func=currently_playing,
             markup=False,
@@ -105,95 +123,102 @@ def init_widgets_list(systray=True, is_laptop=os.getenv("IS_LAPTOP")):
                 "Button1": lambda: qtile.cmd_spawn("playerctl previous"),
                 "Button3": lambda: qtile.cmd_spawn("playerctl next"),
             },
-            background=colors["bg"],
         ),
-    ]
-    widgets.extend(bubble_widget(*audio_widgets))
+    )
 
-    if systray:
-        widgets.append(widget.Systray(**widget_defaults, background=transparent))
+
+def systray():
+    return modify(widget.Systray, **base(bg=transparent))
+
+
+def groups():
+    return modify(
+        GroupBox,
+        **base(bg=transparent, padding=2),
+        highlight_method="text",
+        borderwidth=0,
+        active=colors["fg"],
+        inactive=colors["bg"],
+        this_current_screen_border=colors["green"],
+        this_screen_border=colors["green"],
+        other_screen_border=colors["fg"],
+        other_current_screen_border=colors["fg"],
+        disable_drag=True,
+        use_mouse_wheel=False,
+    )
+
+
+def cpu() -> list:
+    return wrap_widgets(
+        modify(
+            widget.CPU,
+            **base(),
+            format="CPU {load_percent}%",
+            update_interval=1,
+        ),
+        modify(widget.TextBox, **base(padding=2), text="|"),
+        modify(
+            widget.ThermalSensor,
+            **base(),
+            tag_sensor="Package id 0" if os.getenv("IS_LAPTOP") else "Tctl",
+            threshold=90,
+        ),
+    )
+
+
+def mem():
+    return wrap_widgets(
+        modify(
+            widget.Memory,
+            **base(),
+            measure_mem="G",
+            format="{MemUsed:.2f} GB",
+        )
+    )
+
+
+def battery():
+    return wrap_widgets(
+        modify(
+            widget.Battery,
+            **base(),
+            charge_char="",
+            discharge_char="",
+            format="{char}{percent:2.0%} {hour:d}:{min:02d} left",
+        )
+    )
+
+
+def net():
+    return wrap_widgets(
+        modify(
+            widget.Net,
+            **base(),
+            format="{down} ↓↑{up}",
+        )
+    )
+
+
+def init_widgets_list(show_systray=True):
+    widgets = []
+    widgets.extend(clock("%A, %B %d"))
+    widgets.extend(audio())
+
+    if show_systray:
+        widgets.append(systray())
 
     widgets.append(widget.Spacer())
-    widgets.append(
-        GroupBox(
-            **widget_defaults,
-            highlight_method="text",
-            borderwidth=0,
-            active=colors["fg"],
-            inactive=colors["bg"],
-            this_current_screen_border=colors["green"],
-            this_screen_border=colors["green"],
-            other_screen_border=colors["fg"],
-            other_current_screen_border=colors["fg"],
-            disable_drag=True,
-            use_mouse_wheel=False,
-            background=transparent,
-        )
-    )
+    widgets.append(groups())
     widgets.append(widget.Spacer())
 
-    widgets.extend(
-        bubble_widget(
-            widget.CPU(
-                **widget_defaults,
-                format="CPU {load_percent}%",
-                update_interval=1.0,
-                background=colors["bg"],
-            ),
-            widget.TextBox(**widget_defaults, text="|", background=colors["bg"]),
-            widget.ThermalSensor(
-                **widget_defaults,
-                tag_sensor="Package id 0" if is_laptop else "Tctl",
-                threshold=90,
-                background=colors["bg"],
-            ),
-        )
-    )
+    widgets.extend(cpu())
+    widgets.extend(mem())
+    widgets.extend(net())
 
-    widgets.extend(
-        bubble_widget(
-            widget.Memory(
-                **widget_defaults,
-                background=colors["bg"],
-                measure_mem="G",
-                format="{MemUsed:.2f} GB",
-            )
-        )
-    )
+    if os.getenv("IS_LAPTOP"):
+        widgets.extend(battery())
 
-    widgets.extend(
-        bubble_widget(
-            widget.Net(
-                **widget_defaults,
-                format="{down} ↓↑{up}",
-                background=colors["bg"],
-            )
-        )
-    )
-
-    if is_laptop:
-        widgets.extend(
-            bubble_widget(
-                widget.Battery(
-                    **widget_defaults,
-                    charge_char="",
-                    discharge_char="",
-                    format="{char}{percent:2.0%} {hour:d}:{min:02d} left",
-                    background=colors["bg"],
-                )
-            )
-        )
-
-    widgets.extend(
-        bubble_widget(
-            widget.Clock(
-                **widget_defaults,
-                format="%I:%M %p",
-                background=colors["bg"],
-            ),
-            sep=False,
-        )
-    )
+    widgets.extend(clock(format="%I:%M %p"))
     return widgets
 
 
@@ -205,7 +230,6 @@ screens = [
             size=25,
             margin=[10, 10, 5, 10],
             background=transparent,
-            foreground=colors["fg"],
         ),
         bottom=bar.Gap(5),
         left=bar.Gap(5),
@@ -213,12 +237,11 @@ screens = [
     ),
     Screen(
         top=bar.Bar(
-            widgets=init_widgets_list(systray=False),
+            widgets=init_widgets_list(show_systray=False),
             opacity=1,
             size=25,
             margin=[10, 10, 5, 10],
             background=transparent,
-            foreground=colors["fg"],
         ),
         bottom=bar.Gap(5),
         left=bar.Gap(5),
